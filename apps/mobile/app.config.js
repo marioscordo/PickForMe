3f8cc31d25d75800fdafc53a71f4a8181ef2bcb7
Mario Scordo <mario.scordo@t-online.de>
2026-07-03 13:56:13 +0200

    chore: record iOS production build metadata

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -22,13 +22,14 @@ module.exports = {
 
     ios: {
       supportsTablet: false,
-      buildNumber: "1.0.0",
+      buildNumber: "1.0.1",
       bundleIdentifier: IS_PROD
         ? "com.marioscordo.gustaroai"
         : "com.marioscordo.gustaroai.dev",
 
       infoPlist: {
         NSCameraUsageDescription: cameraUsageDescription,
+        ITSAppUsesNonExemptEncryption: false,
         ...(!IS_PROD
           ? {
               NSAppTransportSecurity: {
