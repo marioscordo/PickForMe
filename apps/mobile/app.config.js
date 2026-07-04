4148711f59edd731a2b68f18223569ca1bd8aa3f
Mario Scordo <mario.scordo@t-online.de>
2026-07-04 07:59:38 +0200

    chore: fix ios build and submit config

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
     ios: {
       supportsTablet: false,
-      buildNumber: "1.0.4",
+      buildNumber: "1.0.5",
       bundleIdentifier: IS_PROD
         ? "com.marioscordo.gustaroai"
         : "com.marioscordo.gustaroai.dev",
