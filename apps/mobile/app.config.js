376b210fae836a7e33853b17927ccb293febbc3f
Mario Scordo <mario.scordo@t-online.de>
2026-07-05 09:02:26 +0200

    chore(release): bump iOS build to 1.0.8

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
     ios: {
       supportsTablet: false,
-      buildNumber: "1.0.7",
+      buildNumber: "1.0.8",
       bundleIdentifier: IS_PROD
         ? "com.marioscordo.gustaroai"
         : "com.marioscordo.gustaroai.dev",
