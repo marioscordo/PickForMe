8b99a71feee8ba34338d6c74421780482ba00886
Mario Scordo <mario.scordo@t-online.de>
2026-07-06 09:30:08 +0200

    Prepare iOS build 1.0.10

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
     ios: {
       supportsTablet: false,
-      buildNumber: "1.0.9",
+      buildNumber: "1.0.10",
       bundleIdentifier: IS_PROD
         ? "com.marioscordo.gustaroai"
         : "com.marioscordo.gustaroai.dev",
