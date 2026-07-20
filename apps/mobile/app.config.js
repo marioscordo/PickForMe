18d4ec65888ee2f9fe9a9368a86d715f03814557
Mario Scordo <mario.scordo@t-online.de>
2026-07-20 13:48:47 +0200

    chore: set ios build number to 1.0.20

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
     ios: {
       supportsTablet: false,
-      buildNumber: "1.0.19",
+      buildNumber: "1.0.20",
       bundleIdentifier: IS_PROD
         ? "com.marioscordo.gustaroai"
         : "com.marioscordo.gustaroai.dev",
